81597932660fcc1c5b71046b342dbbf8c0053e80
Shimork04 <mauryashivampersbiz@gmail.com>
2024-03-25 02:18:48 +0530

    updated commits

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector(".changeColorBtn");
+const colorgrid = document.getElementById( "colorGrid" );
+const colorValue = document.getElementById("colorValue");
 
+
+// color-picker popup function
 btn.addEventListener("click", async () => {
   console.log("Pick Color button clicked");
 
@@ -9,6 +13,22 @@ btn.addEventListener("click", async () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: pickColor,
+  }, async(injectionResults) =>{
+
+    // send chosen color back to the content script and then to the background page for storage in localStorage
+    console.log(injectionResults); 
+    const [data] = injectionResults;   
+    if(data.result)
+    {
+        const color = data.result.sRGBHex;
+        colorgrid.style.background = color;
+        colorValue.value=color;
+    }
+    else
+    {
+        alert('Failed to get the color');
+    };
+
   });
 });
 
@@ -17,7 +37,7 @@ async function pickColor() {
   try {
     //pikcer
     const eyeDropper = new EyeDropper();
-    const selectedColor = await navigator.eyeDropper.open();
+    return await eyeDropper.open();
     console.log(selectedColor);
 
   } catch (err) {
